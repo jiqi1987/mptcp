fab185cefab20b40551146e42c1f99b83ad0e052
Qi Ji <jiqi1987@yahoo.com>
2017-10-30 08:20:49 +0000

    mptcp server

--- server/mptcp_server.cpp
+++ server/mptcp_server.cpp
@@ -9,6 +9,7 @@
 #include "base/event_loop.h"
 #include "protocol/mptcp_test_protocol.h"
 #include "network/address_utility.h"
+#include "network/tcp_connection.h"
 #include "network/tcp_server.h"
 
 using std::cout;
@@ -19,6 +20,7 @@ using namespace agora::network;  // NOLINT
 
 class MptcpServer :
   private TcpServerCallback,
+  private TcpConnectionCallback,
   public testing::Test {
  public:
   MptcpServer() :
@@ -39,17 +41,42 @@ class MptcpServer :
  protected:
   virtual void OnConnectionAccepted(TcpConnection *conn);
 
+  virtual void OnError(TcpConnection *conn, int events);
+
+  virtual void OnPacketReceived(TcpConnection *conn, Unpacker *p,
+                                uint16_t service_type, uint16_t uri);
+
  private:
   // static const string server_ip = "10.10.114.52";
   // static const uint16_t server_port = 10913;
 
  private:
   EventLoop loop_;
+  TcpConnection *conn_;
   TcpServer server_;
 };
 
 void MptcpServer::OnConnectionAccepted(TcpConnection *conn) {
   cout << "OnConnectionAccpeted" << endl;
+  conn_ = conn;
+  conn_->RegisterCallback(this);
+}
+
+void MptcpServer::OnError(TcpConnection *conn, int events) {
+}
+
+void MptcpServer::OnPacketReceived(TcpConnection *conn, Unpacker *pkr,
+                                   uint16_t service_type, uint16_t uri) {
+  if (uri == 3) {
+    TestData data(1);
+    data.Unmarshall(pkr);
+    cout << ".";
+  }
+  if (uri == 1) {
+    cout << "|";
+    Pong pong(1);
+    conn_->SendPacket(pong);
+  }
 }
 
 TEST_F(MptcpServer, server) {
